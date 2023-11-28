10d122ee4011f2276f7f79911991d60491b737a8
akhialomgir <akhialomgir362856@gmail.com>
2023-11-28 16:49:16 +0800

    删除nvim配置

--- Linux/cfgs/nvim/lua/plugins/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-    { import = "plugins.common" },
-    { import = "plugins.git" },
-    { import = "plugins.lsp" },
-    { import = "plugins.theme" },
-    { import = "plugins.treesitter" },
-    { import = "plugins.ui" },
-}

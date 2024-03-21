87a5ebc5b9799bf43e93c095a369430e594e06e1
AKHIA <50533365+akhialomgir@users.noreply.github.com>
2024-03-21 08:43:21 +0800

    Update config.js

--- Chrome/cfgs/Surfingkeys/config.js
+++ Chrome/cfgs/Surfingkeys/config.js
@@ -57,33 +57,6 @@ api.Front.registerInlineQuery({
         }
     }
 });
-Front.registerInlineQuery({
-    url: function(q) {
-        return `http://dict.youdao.com/w/eng/${q}/#keyfrom=dict2.index`;
-    },
-    parseResult: function(res) {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(res.text, "text/html");
-        var collinsResult = doc.querySelector("#collinsResult");
-        var authTransToggle = doc.querySelector("#authTransToggle");
-        var examplesToggle = doc.querySelector("#examplesToggle");
-        if (collinsResult) {
-            collinsResult.querySelectorAll("div>span.collinsOrder").forEach(function(span) {
-                span.nextElementSibling.prepend(span);
-            });
-            collinsResult.querySelectorAll("div.examples").forEach(function(div) {
-                div.innerHTML = div.innerHTML.replace(/<p/gi, "<span").replace(/<\/p>/gi, "</span>");
-            });
-            var exp = collinsResult.innerHTML;
-            return exp;
-        } else if (authTransToggle) {
-            authTransToggle.querySelector("div.via.ar").remove();
-            return authTransToggle.innerHTML;
-        } else if (examplesToggle) {
-            return examplesToggle.innerHTML;
-        }
-    }
-});
 
 // set theme
 const hintsCss =

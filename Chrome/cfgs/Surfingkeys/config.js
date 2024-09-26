7aaf69612d12d5cc4ddefbb9a497f64e98804925
AKHIA <50533365+akhialomgir@users.noreply.github.com>
2024-09-26 12:25:47 +0800

    Update config.js

--- Chrome/cfgs/Surfingkeys/config.js
+++ Chrome/cfgs/Surfingkeys/config.js
@@ -467,4 +467,4 @@ addSearchAlias('y', 'yt', 'https://invidious.snopyta.org/search?q=', 's');
 
 // Except
 api.unmapAllExcept(['E','R','T'], /gmail.com|twitter.com/);
-api.unmapAllExcept(['E','R','T'], /leetcode.cn/);
+api.unmapAllExcept([], /leetcode.cn/);
